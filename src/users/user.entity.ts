0db2644ec712f4f07e113a7db779123a9a3086c6
David Rosero Calle <davidroserocalle@gmail.com>
2024-02-04 11:38:30 -0500

    Add user CRUD operations and update user DTO

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -17,19 +17,4 @@ export class User {
 
   @Column()
   password: string;
-
-  @AfterInsert()
-  logInsert() {
-    console.log('Inserted User with id', this.id);
-  }
-
-  @AfterUpdate()
-  logUpdate() {
-    console.log('Updated User with id', this.id);
-  }
-
-  @AfterRemove()
-  logRemove() {
-    console.log('Removed User with id', this.id);
-  }
 }
